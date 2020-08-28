a0be8ae5b6b6f681a3768b9666e4831d9efcfbff
kazuhmiyara <69708052+kazuhmiyara@users.noreply.github.com>
2020-08-28 22:47:19 +0900

    配列修正済み

--- Task1_4.java
+++ Task1_4.java
@@ -17,7 +17,7 @@ public class Task1_4 {
         String [] country = {"JAPAN","AMERICA","KOREA"};
 
         // ② ①で作成した配列の要素数を出力してください。
-        System.out.println(country.length);
+        System.out.println(contry.length);
         /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
          *   1番目（先頭）の要素に 「りんご」 を代入
          *   2番目の要素に 「もも」 を代入
