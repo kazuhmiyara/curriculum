e93ca2a9ac69ae7a0ec5f1455b2e01827cc23375
kazuhmiyara <69708052+kazuhmiyara@users.noreply.github.com>
2020-08-29 15:21:41 +0900

    制御分（条件分岐）修正しました。

--- Task1_6.java
+++ Task1_6.java
@@ -15,8 +15,8 @@ public class Task1_6 {
     //   final修飾子を付与すると、変数（動的に変わる値）ではなく、定数（変更不可な定まった値）となります。
     //   「ここへ記述」部分へ適当な値を記述しましょう。
     //   ※詳しくはJava2-10のwikiにて紹介します。
-    private static final String CONST_ALICE = " Java";
-    private static final String CONST_PASS = "JavaScript";
+    private static final String CONST_ALICE = " alice ";
+    private static final String CONST_PASS = " pass ";
 
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
@@ -24,7 +24,7 @@ public class Task1_6 {
     private static final String CONST_MSG_EROOR_PASS= "パスワードに誤りがあります。";
     private static final String CONST_MSG_ERROR_INPUT = "入力情報に誤りがあります。";
 
-    public static void main(String[] args) {
+
 
         // 以下の変数「name（名前）」「pass（パスワード）」を使用して、②〜⑤の条件を満たす処理を記述してください。
         // 名前とパスワードは画面からの入力値だと想定しsてみましょう。
@@ -33,7 +33,7 @@ public class Task1_6 {
 
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
         if (name.equals("alice") && pass.equals("pass")) {
-        	System.out.println( CONST_MSG_SUCCESS);
+        	System.out.println(CONST_MSG_SUCCESS);
 
         }
 
